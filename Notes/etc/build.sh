409bf0e2aa89449bd561d4e052f13211e71a760f
Constantine Theocharis <kontheocharis@gmail.com>
2018-10-21 20:17:25 +0200

    Fix bugs in Notes

--- Notes/etc/build.sh
+++ Notes/etc/build.sh
@@ -3,15 +3,16 @@ DIR_NAME=$(basename "$PWD")
 EXT_FORMAT=$(cat settings/format | head -n 1)
 OUT_FORMAT=$(cat settings/format | tail -n 1)
 ENGINE=$(cat settings/engine)
-FILTERS=" "
+FILTERS=' '
 
 if [ -s settings/filters ]
 then
-   FILTERS="--filter $(cat settings/filters)"
+    FILTERS=$(cat settings/filters | awk '{print "--filter " $0}')
 fi
 
 pandoc $($LAYOUT) --pdf-engine=pdflatex \
        -t $OUT_FORMAT \
        --include-in-header etc/header-includes.tex \
+       --include-in-header settings/header-includes.tex \
        -o "out/$DIR_NAME.$EXT_FORMAT" \
        $FILTERS
